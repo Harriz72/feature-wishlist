c34db5ec9b8a6c36b3900f346df264665bff2375
Harriz72 <122156749+Harriz72@users.noreply.github.com>
2025-01-07 16:39:55 +0800

    Backend API

--- features.php
+++ features.php
@@ -1,3 +1,9 @@
 echo "Created database schema for wishlist items" >> commit-log.txt
 git add commit-log.txt
 git commit -m "feat: add database schema for wishlist"
+
+
+
+echo "Implemented backend API for managing wishlist" >> commit-log.txt
+git add commit-log.txt
+git commit -m "feat: add backend API for wishlist"
