80f050a6b84be0c85e504162fb383b55f5f48b9c
Harriz72 <122156749+Harriz72@users.noreply.github.com>
2025-01-07 16:40:15 +0800

    frontend logic

--- features.php
+++ features.php
@@ -7,3 +7,9 @@ git commit -m "feat: add database schema for wishlist"
 echo "Implemented backend API for managing wishlist" >> commit-log.txt
 git add commit-log.txt
 git commit -m "feat: add backend API for wishlist"
+
+
+
+echo "Added frontend logic for displaying wishlist" >> commit-log.txt
+git add commit-log.txt
+git commit -m "feat: add frontend logic for wishlist"
